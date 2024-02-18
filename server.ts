6e076c2f6f51536ab4d253c5af8575a54d2a1270
kei95 <kei0613shine.star@gmail.com>
2024-02-18 13:49:15 -0800

    Initial commit

---
+++ server.ts
@@ -0,0 +1,48 @@
+import express from "express";
+import { pool } from "./db";
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  try {
+    const data = await pool.query("SELECT * FROM schools");
+    res.status(200).send(data.rows);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/", async (req, res) => {
+  const { name, location } = req.body;
+  try {
+    await pool.query("INSERT INTO schools (name, address) VALUES ($1, $2)", [
+      name,
+      location,
+    ]);
+    res.status(200).send({ message: "Successfully added child" });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.get("/setup", async (req, res) => {
+  try {
+    await pool.query(
+      "CREATE TABLE schools ( id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100))",
+    );
+    res.status(200).send({ message: "Successfully created table" });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`the server has started on port: ${PORT}`);
+});
